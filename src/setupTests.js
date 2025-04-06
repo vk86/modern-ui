f72dbe3d349ab9de3d4a73a81323a02839b92c1a
vk86 <nvinoth2007@gmail.com>
2025-04-06 00:30:42 -0500

    Update setupTests.js

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,18 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import SearchableDropdown from './SearchableDropdown'; // make sure it's imported
+
+<Form.Group className="mb-3 pt-3 w-100">
+  <Form.Label><b>Application</b></Form.Label>
+
+  <SearchableDropdown
+    label="Select Application"
+    options={isRallyAuthenticated && projects.length > 0 ? projects : []}
+    onSelect={handleApplicationChange}
+    disabled={!isRallyAuthenticated || projects.length === 0}
+  />
+
+  {!isRallyAuthenticated || projects.length === 0 ? (
+    <div style={{ color: '#999', fontSize: '13px', marginTop: '4px' }}>
+      No projects available
+    </div>
+  ) : null}
+</Form.Group>
